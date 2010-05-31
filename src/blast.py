a3eccba55da0e5a54d61fd41dd9f2890c2f488a9
rodrigo.faccioli <rodrigo.faccioli@gmail.com>
2010-05-31 17:45:36 -0300

    Fixed Fasta error. Remove Bio.writers.SeqRecord.fasta

--- src/blast.py
+++ src/blast.py
@@ -3,7 +3,6 @@ from Bio.Blast import NCBIXML
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
-import Bio.writers.SeqRecord.fasta
 import Bio.Alphabet
 import sys
 import os
